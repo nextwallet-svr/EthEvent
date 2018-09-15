8baa17ee84351a6df3c245ca0b9c85a3c141d12f
rockercheng <8350088@qq.com>
2018-09-15 11:41:25 +0800

    upd

--- EthEvent.py
+++ EthEvent.py
@@ -116,11 +116,11 @@ def destroyEventFilters():
 def callEventHandlers(_abi, all = 1):
     try:
         for name, event_filter in g_event_filter_map.items():
-        	events = {}
-        	if (all == 1):
-            	events = event_filter.get_all_entries()
+            events = {}
+            if (all == 1):
+                events = event_filter.get_all_entries()
             else:
-            	events = event_filter.get_new_entries()
+                events = event_filter.get_new_entries()
 
             if (events is None or len(events) == 0):
                 debug('events empty, name: %s', name)
